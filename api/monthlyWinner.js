53ad65ab0dd52775a17338a3841a21ba05f1aa46
Kim Bum June <bjbj6363@gmail.com>
2018-07-29 02:27:01 +0900

    updated monthly winner

---
+++ api/monthlyWinner.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const AWS = require("aws-sdk");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const table = "monthlyWinner";
+
+module.exports.put = (event, context, callback) => {
+  const data = event.body;
+  const putQuery = {
+    TableName: table,
+    Item: {
+      season: data.season,
+      winners: { ...data.winners }
+    }
+  };
+  dynamoDb.put(putQuery, (err, data) => {
+    if (err) {
+      callback(null, {
+        statusCode: 403,
+        body: err
+      });
+    }
+    callback(null, {
+      statusCode: 200,
+      body: "created!"
+    });
+  });
+};
+
+module.exports.get = (event, context, callback) => {
+  const data = event.pathParameters.id;
+  const getQuery = {
+    TableName: table,
+    Key: {
+      id: data
+    }
+  };
+  dynamoDb.get(getQuery, (err, data) => {
+    if (err) {
+      callback(null, {
+        statusCode: err.statusCode || 501,
+        headers: { "Content-Type": "text/plain" },
+        body: "데이터를 가져올 수 없습니다"
+      });
+    }
+    callback(null, {
+      statusCode: 200,
+      headers: { "Content-Type": "text/plain" },
+      body: JSON.stringify(data.Item)
+    });
+  });
+};
